9d3781918c72342b486f62d077daa6dade9e4844
Aswin110 <aswinashok110@gmail.com>
2023-08-30 07:43:29 +0530

    4 operators are working properly and added clear button

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const operands = document.querySelectorAll(".operand");
 const clear = document.querySelector(".clear");
 const sign = document.querySelector(".sign");
 const percentage = document.querySelector(".percentage");
+const equalTo = document.querySelector(".equalTo");
 
 let firstNumber = ""; 
 let operator1 = "";
 let secondNumber = ""; 
+let result = 0;
 
 operands.forEach(function(operand){
         operand.addEventListener('click',function(){
@@ -32,4 +34,32 @@ clear.addEventListener('click',function(){
     firstNumber = "";
     operator1 = "";
     secondNumber = "";
-})
+});
+
+equalTo.addEventListener('click',operate)
+
+function operate(){
+    if(firstNumber !== "" && operator1 !== "" && secondNumber !== "" ){
+        let num1 = parseFloat(firstNumber);
+        let num2 = parseFloat(secondNumber);
+        
+        switch(operator1){
+            case  '-':
+                result = num1 - num2;
+                break;
+            case  '+':
+                result = num1 + num2;
+                break;
+            case  '*':
+                result = num1 * num2;
+                break;
+            case  '/':
+                result = num1 / num2;
+                break;
+        }
+        document.getElementById("display").innerText = `${result}`
+    }
+}
+
+
+
